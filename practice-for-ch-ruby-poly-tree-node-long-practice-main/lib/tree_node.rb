8f750a1fe22699659a0af48e33e5b35bb22dfba7
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-04 14:28:19 -0700

    Finish parent= method

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -18,14 +18,16 @@ class PolyTreeNode
     end
     
     def parent=(new_parent) # nil or an instance
+        @parent.children.delete(self) if @parent != nil && @parent.children.include?(self)  
         @parent = new_parent # if @parent == nil
         if new_parent == nil   
             return nil
         elsif !new_parent.children.include?(self) # || self.parent == nil
             new_parent.children << self
-            @parent.children.delete(self)
         end
     end
+
+    
 
     def inspect
         return "<# Value: #{@value} Parent: #{@parent} Children: #{@children} "
