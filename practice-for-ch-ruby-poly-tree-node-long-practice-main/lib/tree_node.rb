1322d3b97577f2c53f534b0d91ea47e418f296f9
Shannon Millar <shanenak@gmail.com>
2023-08-04 12:11:19 -0700

    Add initialize function

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,22 @@
 class PolyTreeNode
-  
+    def initialize(value)
+        @parent = nil # instance
+        @children = [] # array of instances
+        @value = value # string
+    end
+
+    def parent
+        @parent
+    end
+    def children
+        @children
+    end
+    def value
+        @value
+    end
+    
+    def parent=(new_parent) # nil or an instance
+        @parent = new_parent if !@parent
+        new_parent.children << self
+    end
 end
